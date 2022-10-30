17f256f0509d70e431cd092729ec25b03ceae9bf
MontseOrtiz <montse_2908@hotmail.com>
2022-10-30 17:59:28 -0600

    Adding problem 6 solved

---
+++ problem6.js
@@ -0,0 +1,38 @@
+// Problem 6
+
+// The sum of the squares of the first ten natural numbers is,
+
+// The square of the sum of the first ten natural numbers is,
+
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+const sumSquares = (num) => {
+  const squares = [];
+  for (let i = 1; i <= num; i++) {
+    squares.push(Math.pow(i,2))
+  }
+  const total = squares.reduce((a, b) => a + b, 0)
+  return total
+}
+
+const sumSquaresDouble = (num) => {
+  const numbers = [];
+  for (let i = 1; i <= num; i++) {
+    numbers.push(i)
+  }
+  const total = numbers.reduce((a, b) => a + b, 0)
+  const totalSquare = Math.pow(total, 2)
+  return totalSquare
+}
+
+
+const difference = (num) =>{
+  return sumSquaresDouble(num) - sumSquares(num)
+}
+
+console.log(sumSquares(10))
+console.log(sumSquaresDouble(10))
+console.log(difference(100))
